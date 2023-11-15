f5e1e552692d8be586e0e8a71ee3b62b24b76637
alvesjaov <victoralves169@outlook.com>
2023-11-15 15:46:06 -0300

    Add: PropTypes aos componentes Button e Input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 import './InputStyles.css';
 
 function Input({ type, placeholder, onChange }) {
     return <input className='Input' type={type} placeholder={placeholder} onChange={onChange} />;
 }
+
+Input.propTypes = {
+    type: PropTypes.string, //Se uma prop não for marcada como isRequired, ela é considerada opcional. Se uma prop opcional não for fornecida, ela será undefined
+    placeholder: PropTypes.string,
+    onChange: PropTypes.func
+};
 
 export default Input;
