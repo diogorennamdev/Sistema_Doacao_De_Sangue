3aa92da20d797c6acd5ef6f90b20eb25beb5fa0f
diogorennam <diogorenan006@gmail.com>
2023-11-16 22:32:18 -0300

    fix: implementação para testar o useContext

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,23 @@
-import './styles.css'
+import React, { useContext } from 'react';
+import { useAuth } from '../../Contexts/useContext';
+import './styles.css';
 
 function Home() {
+  const { userData, logout } = useAuth(); // Obtendo os dados do contexto
+
   return (
     <div>
       <h2>Home</h2>
-    
+      {userData ? (
+        <div>
+          <p>Welcome, {userData.token}!</p>
+          <button onClick={logout}>Logout</button>
+        </div>
+      ) : (
+        <p>Please login to view this content.</p>
+      )}
     </div>
-
-  )
+  );
 }
 
-export default Home
+export default Home;
