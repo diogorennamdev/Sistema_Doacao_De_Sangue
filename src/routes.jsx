0415c3f924d5fd5bf7fc4df45fcc417f73b6ae75
diogorennam <diogorenan006@gmail.com>
2023-11-16 22:28:20 -0300

    fix: Implementação de telas Protegidas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,34 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider, useAuth } from './Contexts/useContext';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Admin from './pages/Admin';
+
+function PrivateRoute({ element: Element, ...rest }) {
+  const { getUserData } = useAuth();
+  const userData = getUserData();
+  const isLoggedIn = userData !== null;
+
+  return isLoggedIn ? <Element {...rest} /> : <Navigate to="/" replace />;
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>} />
-        <Route path='/Home' element={<Home/>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/home"
+            element={<PrivateRoute element={Home} />}
+          />
+          <Route
+            path="/admin"
+            element={<PrivateRoute element={Admin} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
