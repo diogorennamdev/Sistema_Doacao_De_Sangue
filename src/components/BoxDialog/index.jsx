298712e937a7225cdfd671c0485b735559a90683
alvesjaov <victoralves169@outlook.com>
2023-11-17 11:42:17 -0300

    Add: backdrop ao modal BoxDialog

--- src/components/BoxDialog/index.jsx
+++ src/components/BoxDialog/index.jsx
@@ -1,17 +1,18 @@
 import { Modal } from 'react-bootstrap';
 import Button from '../Button';
 import PropTypes from 'prop-types';
+
 import './styles.css'; // Importando o arquivo CSS
 
 function BoxDialog({ show, handleClose, title, message }) {
     return (
-        <Modal show={show} onHide={handleClose} className="my-modal">
+        <Modal show={show} onHide={handleClose} className="my-modal" backdrop="static">
             <Modal.Header closeButton>
                 <Modal.Title>{title}</Modal.Title>
             </Modal.Header>
             <Modal.Body>{message}</Modal.Body>
             <Modal.Footer>
-            <Button variant="primary" onclick={handleClose} TextButton={'Fechar'} />
+                <Button variant="primary" onclick={handleClose} TextButton={'Fechar'} />
             </Modal.Footer>
         </Modal>
     );
