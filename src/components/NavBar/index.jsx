2e61e4260862356008a87399a4f0425179558188
alvesjaov <victoralves169@outlook.com>
2023-11-20 13:16:46 -0300

    Add: componente Logo ao NavBar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,14 +1,19 @@
 import { useLocation } from 'react-router-dom';
+import Logo from '../Logo';
+import './styles.css';
 
 function Navbar() {
   const location = useLocation();
-  
+
   if (location.pathname === './src/pages/login' || location.pathname === '/') {
     return null;
   }
   return (
-    <>      
-    </>
+
+    <div className="ContainerHome">
+      <Logo />
+    </div>
+
 
   );
 }
