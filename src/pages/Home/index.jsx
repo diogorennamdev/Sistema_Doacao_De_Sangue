1a7686aef27ddef61d28b851bda3e0090efa2dd5
alvesjaov <victoralves169@outlook.com>
2023-11-21 01:04:11 -0300

    Refactor: o componente Home e adicione botões

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,48 @@
 import { useAuth } from '../../Contexts/useAuth';
+import Button from '../../components/Button';
+import { useMemo } from 'react';
+import { Link } from 'react-router-dom';
+
+
 import './styles.css';
 
 function Home() {
-  const { userData, logout } = useAuth(); // Obtendo os dados do contexto
+  const { userData } = useAuth(); // Obtendo os dados do contexto
+  const loading = useMemo(() => false, []);
 
   return (
-    <div>
-      <h2>Inicio</h2>
-      {userData ? (
-        <div>
-          <p>Bem-Vindo, {userData.dados.Nome}!</p>
-          <button onClick={logout}>Sair</button>
+    <main>
+      <div className='container'>
+        <h2 className='title'>INFORMAÇÕES DO HOMOCENTRO</h2>
+        {userData ? (
+          <div className='welcome'>
+            <p>Bem-Vindo, {userData.dados.Nome}!</p>
+
+          </div>
+        ) : (
+          <p>Você não está logado.</p>
+        )}
+        <div className='containerButton' >
+          <Button
+            loading={loading}
+            TextButton={'Gerenciar Doadores'}
+          />
+          <Button
+            loading={loading}
+            TextButton={'Adicionar Exames'}
+          />
+          <Button
+            loading={loading}
+            TextButton={<Link to="/stock">ESTOQUE</Link>}
+          />
+          <Button
+            loading={loading}
+            TextButton={'Cadastro de funcionários'}
+          />
         </div>
-      ) : (
-        <p>Você não está logado.</p>
-      )}
-    </div>
+        
+      </div>
+    </main>
   );
 }
 
