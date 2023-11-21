47949808099a8f9c3e3171503e296056cd1c830f
João Victor Alves <victoralves169@outlook.com>
2023-11-21 14:39:58 -0300

    Refactor: a página de estoque para exibir ícones de bolsas de sangue e porcentagens na tabela

--- src/pages/Stock/index.jsx
+++ src/pages/Stock/index.jsx
@@ -1,6 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useAuth } from '../../Contexts/useAuth';
 import axios from 'axios';
+import adequado from '../../images/adequado.svg';
+import alerta from '../../images/alerta.svg';
+import critico from '../../images/critico.svg';
+import estavel from '../../images/estavel.svg';
+
+import './styles.css';
 
 const Stock = () => {
     const stock = import.meta.env.VITE_STOCK
@@ -21,47 +27,68 @@ const Stock = () => {
         fetchCounts();
     }, [stock, userData]);
 
-    // Transforma o objeto counts em um array de arrays, cada um com dois elementos: tipo sanguíneo e contagem
     const countsArray = Object.entries(counts);
-
-    // Calcula o total de bolsas no estoque
     const totalBags = countsArray.reduce((total, [, count]) => total + count, 0);
 
-    // Calcula a porcentagem de bolsas para cada tipo sanguíneo
     const countsWithPercentage = countsArray.map(([bloodType, count]) => {
-        const cleanedBloodType = bloodType.replace(' (Pos)', '').replace(' (Neg)', '');// Remove o Pos e Neg do tipo sanguíneo
+        const cleanedBloodType = bloodType.replace(' (Pos)', '').replace(' (Neg)', '');
         let percentage = 0;
         if (totalBags !== 0) {
-            percentage = ((count / totalBags) * 100).toFixed(2);
+            percentage = Math.floor((count / totalBags) * 100); // Alterado aqui
         }
-        return [bloodType, count, cleanedBloodType, percentage];// Retorna um array com o tipo sanguíneo original, a contagem, o tipo sanguíneo limpo e a porcentagem
+        return [bloodType, count, cleanedBloodType, percentage];
     });
-    
+
     return (
-        <div>
-            {countsWithPercentage.map(([, , cleanedBloodType, percentage]) => (
-                <p key={cleanedBloodType}>{cleanedBloodType}: {percentage}%</p> // Usa o tipo sanguíneo limpo para a porcentagem
-            ))}
+        <div className='ContainerBloodBag'>
             <h1>Estoque de Bolsas de Sangue</h1>
+            <div className="icon-grid">
+                {countsWithPercentage.map(([, , cleanedBloodType, percentage]) => {
+                    let iconSrc, altText;
+                    if (percentage < 10) {
+                        iconSrc = critico;
+                        altText = "Critico";
+                    } else if (percentage < 20) {
+                        iconSrc = alerta;
+                        altText = "Alerta";
+                    } else if (percentage < 60) {
+                        iconSrc = adequado;
+                        altText = "Adequado";
+                    } else {
+                        iconSrc = estavel;
+                        altText = "Estavel";
+                    }
+
+                    return (
+                        <div key={cleanedBloodType} className="icon-container">
+                            <img src={iconSrc} alt={altText} />
+                            <p>{altText}</p>
+                            <p className="icon-text">{cleanedBloodType}</p>
+                        </div>
+                    );
+                })}
+            </div>
+
             <table>
                 <thead>
                     <tr>
-                        {countsWithPercentage.map(([bloodType]) => (
-                            <th key={bloodType}>{bloodType}</th> // Usa o tipo sanguíneo original para a tabela
-                        ))}
+                        <th>Grupo ABO | Rh (Pos) ou (Neg)</th>
+                        <th>Quantidade de bolsas no estoque</th>
+                        <th>Porcentagem %</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        {countsWithPercentage.map(([bloodType, count]) => (
-                            <td key={bloodType}>{count}</td> // Usa o tipo sanguíneo original para a tabela
-                        ))}
-                    </tr>
+                    {countsWithPercentage.map(([, count, cleanedBloodType, percentage]) => (
+                        <tr key={cleanedBloodType}>
+                            <td>{cleanedBloodType}</td>
+                            <td>{count}</td>
+                            <td>{Number.isNaN(percentage) ? 'NaN' : percentage}%</td> 
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
     );
-
 }
 
 export default Stock;
