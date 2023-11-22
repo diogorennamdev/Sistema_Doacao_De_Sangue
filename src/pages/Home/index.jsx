3042702de0ce1a889cdda0d0f8cb64e2070301d8
julioroquesilva <116984649+julioroque@users.noreply.github.com>
2023-11-21 22:31:09 -0300

    Revert "Update: navegação dos botões para as paginas"
    This reverts commit ffd3d496344f71c2ae5792bbb28e04dc78b84fcb.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,63 +1,47 @@
 import { useAuth } from '../../Contexts/useAuth';
 import Button from '../../components/Button';
 import { useMemo } from 'react';
-import { Link } from 'react-router-dom'; // Importe o Link
+import { Link } from 'react-router-dom';
+
 
 import './styles.css';
 
 function Home() {
   const { userData } = useAuth(); // Obtendo os dados do contexto
-  
   const loading = useMemo(() => false, []);
 
   return (
     <main>
-
       <div className='container'>
-
         <h2 className='title'>INFORMAÇÕES DO HOMOCENTRO</h2>
-
         {userData ? (
           <div className='welcome'>
             <p>Bem-Vindo, {userData.dados.Nome}!</p>
+
           </div>
         ) : (
           <p>Você não está logado.</p>
         )}
-
-        <div className='containerButton' > 
-
-          <Link to="/">
-            <Button
-              loading={loading}
-              TextButton={'Gerenciar Doadores'}
-            />
-          </Link>
-
-          <Link to="/">
-            <Button
-              loading={loading}
-              TextButton={'Adicionar Exames'}
-            />
-          </Link>
-
-          <Link to="/Stock">
-            <Button
-              loading={loading}
-              TextButton={'Estoque'}
-            />
-          </Link>
-
-          <Link to="/">
-            <Button
-              loading={loading}
-              TextButton={'Cadastro de funcionários'}
-            />
-          </Link>
+        <div className='containerButton' >
+          <Button
+            loading={loading}
+            TextButton={'Gerenciar Doadores'}
+          />
+          <Button
+            loading={loading}
+            TextButton={'Adicionar Exames'}
+          />
+          <Button
+            loading={loading}
+            TextButton={<Link to="/stock">ESTOQUE</Link>}
+          />
+          <Button
+            loading={loading}
+            TextButton={'Cadastro de funcionários'}
+          />
         </div>
-
+        
       </div>
-
     </main>
   );
 }
