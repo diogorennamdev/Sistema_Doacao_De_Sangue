fa28d93a445a13285b777c78a08e3ef289ba5959
diogorennam <diogorenan006@gmail.com>
2023-11-26 15:28:22 -0300

    fix: Adição da propriedade value no input para manipular dados dele

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 import './styles.css';
 
-function Input({ type, placeholder, onChange }) {
+function Input({ type, placeholder, onChange,value }) {
     return <>
         <input
             className='Input'
             type={type}
             placeholder={placeholder}
             onChange={onChange}
+            value={value}
             autoComplete='on' />
     </>
 }
@@ -15,6 +16,7 @@ function Input({ type, placeholder, onChange }) {
 Input.propTypes = {
     type: PropTypes.string, // Se uma prop não for marcada como isRequired, ela é considerada opcional. Se uma prop opcional não for fornecida, ela será undefined
     placeholder: PropTypes.string,
+    value: PropTypes.string,
     onChange: PropTypes.func
 };
 
