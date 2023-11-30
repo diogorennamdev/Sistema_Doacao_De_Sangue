dabce54e1f43eebdfad5fbcc788bd9e19ea7e514
João Victor Alves <victoralves169@outlook.com>
2023-11-29 23:40:03 -0300

    adicionar verificação de expiração de sessão ao PrivateRoute

--- src/utils/PrivateRoute/index.jsx
+++ src/utils/PrivateRoute/index.jsx
@@ -1,14 +1,44 @@
 import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { useAuth } from "../../Contexts/useAuth";
+import BoxDialog from "../../components/BoxDialog";
+import { useState, useEffect } from "react";
 
 function PrivateRoute({ element: Element, ...rest }) {
   const { getUserData } = useAuth();
   const userData = getUserData();
   const isLoggedIn = userData !== null;
+  const [show, setShow] = useState(false);
+  const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
+  const [navigate, setNavigate] = useState(false);
 
-  return isLoggedIn ? <Element {...rest} /> : <Navigate to="/" replace />;
+  // Verifica se o token expirou
+  const isTokenExpired = userData && userData.information.exp * 1000 < Date.now();
+
+  useEffect(() => {
+    if (!isLoggedIn || isTokenExpired) {
+      setTitle('Sessão expirada');
+      setMessage('Faça login novamente para continuar.');
+      setShow(true);
+    }
+  }, [isLoggedIn, isTokenExpired]);
+
+  // Se o usuário não estiver logado ou o token tiver expirado, redireciona para a página de login
+  const handleClose = () => {
+    setShow(false);
+    setNavigate(true);
+  }
+
+  return (
+    <>
+      {navigate && <Navigate to="/" replace />}
+      <BoxDialog show={show} handleClose={handleClose} title={title} message={message} />
+      {!navigate && <Element {...rest} />}
+    </>
+  );
 }
+
 PrivateRoute.propTypes = {
   element: PropTypes.elementType.isRequired,
   rest: PropTypes.object,
