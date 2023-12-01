118ca4bf22ab2f12c5d761ee65fba96bcc6e44aa
diogorennam <diogorenan006@gmail.com>
2023-12-01 02:45:05 -0300

    fix: adição de lógicas para mostra o modal e as mensagens de sucesso e erro relacionada a atualização da senha

--- src/pages/Employee/index.jsx
+++ src/pages/Employee/index.jsx
@@ -4,6 +4,7 @@ import { useAuth } from '../../Contexts/useAuth';
 import Popup from '../../components/PopUp';
 import Card from '../../components/Card';
 import Input from '../../components/Input';
+import BoxDialog from '../../components/BoxDialog';
 import Loading from '../../components/Loading';
 import { FaSearch } from "react-icons/fa";
 import './styles.css'
@@ -14,10 +15,14 @@ function EmployeeList() {
   const [employees, setEmployees] = useState([]);
   const [loading, setLoading] = useState(true);
   const [show, setShow] = useState(false);
+  const [showBoxDialog, setShowBoxDialog] = useState(false)
   const [userSelected, setUserSelected] = useState('');
-  console.log(userSelected)
   const employee = import.meta.env.VITE_EMPLOYEES;
-
+  const [type, setType] = useState('')
+  const [message, setMessage] = useState('');
+  const [title, setTitle] = useState('');
+  const [personCode, setPersonCode] = useState('');
+  const [password, setPassword] = useState('');
   useEffect(() => {
     setLoading(true); // Ativar o estado de carregamento antes da requisição
 
@@ -43,9 +48,71 @@ function EmployeeList() {
   };
 
   const onClickModal = (user) => {
-   setUserSelected(user); // Atualizar o estado com o usuário clicado
+    setUserSelected(user); // Atualizar o estado com o usuário clicado
     setShow(true);
   };
+
+  const handleCloseBoxDialog = () => {
+    setShowBoxDialog(false);
+    setMessage('');
+    setTitle('');
+    setPassword('')
+    setPersonCode('')
+    setType('')
+
+  };
+
+  //verfico se existe o dado atualizado quando clico em atualizar se sim passo as novas informações e atualizo a o funcionario
+  const onClickUpdate = (password) => {
+    if (!password) {
+      setShowBoxDialog(true);
+      setMessage('É necessário inserir uma nova senha para atualizar!');
+      setTitle('Atenção');
+      return;
+    }
+
+    if (password.length < 8) {
+      setShowBoxDialog(true);
+      setMessage('Sua senha precisa ter no mínimo 8 caracteres!');
+      setTitle('Atenção');
+      return;
+    }
+
+    try {
+      // Aqui, você pode chamar a função employeeUpdate para atualizar a senha
+      employeeUpdate(password);
+    } catch (error) {
+      // Lide com o erro, se necessário
+      console.error(error);
+    }
+  };
+
+  const employeeUpdate = async (password) => {
+    const updatedEmployee = {
+      password: password
+    };
+    try {
+      const response = await axios.patch(`${employee}${userSelected.employeeCode}`, updatedEmployee, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      if (response.status === 200) {
+        setShow(false);
+        setType('success');
+        setShowBoxDialog(true);
+        setMessage('Senha atualizada com sucesso!')
+        setTitle('Sucesso!')
+        setPersonCode(userSelected.employeeCode)
+        setPassword(password);
+       
+      }
+      console.log(response.data); // Se desejar, faça algo com a resposta da requisição
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='ContainerEmployee'>
       <h1>Todos Funcionários</h1>
@@ -64,10 +131,23 @@ function EmployeeList() {
           onClick={(user) => onClickModal(user)}
         />
       )}
+
       <Popup
+        type={'employee'}
         show={show}
         handleClose={handleCloseModal}
         userData={userSelected}
+        onClick={onClickUpdate}
+      />
+
+      <BoxDialog
+        handleClose={handleCloseBoxDialog}
+        show={showBoxDialog}
+        message={message}
+        title={title}
+        type={type}
+        password={password}
+        personCode={personCode}
       />
     </div>
   );
